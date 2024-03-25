3d87d130dc1d5ebac461fe1f0328ce01906434e8
nekokaito <siddiqsazzad001@gmail.com>
2024-03-25 22:10:57 +0600

    NavBar with Links

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,8 +1,12 @@
-
+import { Outlet } from "react-router-dom";
+import NavBar from './navbar/NavBar'
+import Footer from "./footer/Footer";
 const Root = () => {
     return (
         <div>
-            
+            <NavBar></NavBar>
+            <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
